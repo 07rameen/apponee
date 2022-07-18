486ab4e0ac064ce62e618e720d09e8dcfe16e0a4
rameen <rameenhamidd257@gmail.com>
2022-07-18 00:34:16 -0700

    Added about page

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   #root 'application#hello'
   root 'pages#home'
   #method name is defined after the controller name
+  get 'about', to: 'pages#about'
+  #ye static method hai upar wala
 end
